626371da378d38cefd19354b83e6ef938236dd2e
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-10 19:42:12 +0800

    WIP: Initial UI

---
+++ Calculator/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by StartupBuilder.INFO on 9/10/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let BotGray = Color("BBotGray")
+    static let DisplayGray = Color("BDisplayGray")
+    static let TopGray = Color("BTopGray")
+    static let Orange = Color("BOrange")
+}
--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -10,7 +10,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 1) {
+            Text("0")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .foregroundColor(.white)
+                .background(Color.DisplayGray)
+            
+            HStack(spacing: 1) {
+                Presser(value: "AC")
+                Presser()
+                Presser()
+                Presser()
+            }
+            
+            HStack(spacing: 1) {
+                Presser()
+                Presser()
+                Presser()
+                Presser()
+            }
+            
+            HStack(spacing: 1) {
+                Presser()
+                Presser()
+                Presser()
+                Presser()
+            }
+            
+            HStack(spacing: 1) {
+                Presser()
+                Presser()
+                Presser()
+                Presser()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Presser: View {
+    
+    var value: String?
+    
+    var body: some View {
+        Button(action: {
+            
+        }) {
+            Text(value ?? "??")
+                .foregroundColor(.white)
+                .font(.largeTitle)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.BotGray)
     }
 }
 
