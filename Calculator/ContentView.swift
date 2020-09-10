9f511b1ffd645c12755ad9727aeb72158a3e3a6b
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-10 19:10:34 +0800

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by StartupBuilder.INFO on 9/10/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
